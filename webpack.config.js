587f591d2b708549e44214bd714b4b049d1ef35f
Ubuntu <ubuntu@ip-172-31-61-242.ec2.internal>
2020-08-18 20:38:09 +0000

    set up webpack for prd ad updated template html to not defer scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     // website: './client/index.js',
     hydrate: './client/hydrate.js',
