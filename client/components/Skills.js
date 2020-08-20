3aa18eb737f29db227c87f17d422f759af50b279
joshua-t-liu <josh.th.liu@gmail.com>
2020-08-20 01:32:22 -0700

    updated skills UI

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -8,7 +8,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding: 5em 1em;
-  align-items: center;
+  align-items: stretch;
   text-align: center;
   background-color: rgb(247,247,247);
   @media (max-width: ${SMALL_WIDTH}) {
@@ -21,117 +21,98 @@ const Title = styled.h2`
   margin: 0;
 `;
 
-const SubTitle = styled.div`
-  font-size: 1.5em;
-  margin-top: 1em;
-`;
-
-const Groups = styled.div`
+const SkillSets = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  margin: 1em 0;
-`;
-
-const rotate = (to) => keyframes`
-  to {
-    transform: rotate3d(0, 1, 0, ${to}deg);
-  }
-`;
-
-const SkillSetTitle = styled.div`
-  @media (max-width: ${SMALL_WIDTH}) {
-    align-self: flex-start;
-  }
-`;
-
-const SkillSet = styled.div`
-  display: flex;
-  width: 10em;
-  height: 6em;
-  flex-direction: column;
-  align-items: center;
-  font-size: 2em;
-  font-weight: bold;
-  border-radius: 0.25em;
-  padding: 1em;
-  margin 1em;
-  box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 12px 0px, rgba(0, 0, 0, 0.16) 0px 47px 46px -27px;
-  background-color: ${({ flip }) => (flip) ? `rgb(255,255,255)` : 'dodgerblue'};
-  justify-content: ${({ flip }) => !flip && `center`};
-  transform: ${({ flip }) => `rotate3d(0, 1, 0, ${(flip) ? 90 : 0}deg)`};
-  & > ${SkillSetTitle} {
-    margin: ${({ flip }) => !flip && `auto`};
-  }
-  &.active {
-    animation: ${({ flip }) => css`${rotate((flip) ? 0 : -90)} 0.5s linear 0s forwards`};
-  }
-  & > ${SkillSetTitle} {
-    color: ${({ flip }) => (flip) ? 'dodgerblue' : 'white'};
-  }
+  margin-top: 3em;
   @media (max-width: ${SMALL_WIDTH}) {
     margin-top: 1em;
   }
 `;
 
+const SkillSetTitle = styled.div`
+  color: dodgerblue;
+  transform: translateY(5em);
+  opacity: 0;
+  @media (max-width: ${SMALL_WIDTH}) {
+    align-self: flex-start;
+  }
+  transition: transform 0.5s ease-in-out 0.25s,opacity 0.5s ease-in-out 0.25s};
+`;
+
 const Skills = styled.p`
   font-size: 0.75em;
   font-weight: normal;
-  line-height: 1.75em;
+  line-height: 2em;
+  margin: 0;
+  transform: translateY(5em);
+  opacity: 0;
+  transition: ${({ delay }) => css`transform 0.5s ease-in-out ${(1 + delay)/8 + 0.25}s,opacity 0.5s ease-in-out ${(1 + delay)/8 + 0.25}s`};
+`;
+
+const SkillSet = styled.div`
+  width: 20%;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2em;
+  font-weight: bold;
   text-align: left;
+  &.active {
+    & > div > ${SkillSetTitle} {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    & > div > ${Skills} {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  @media (max-width: ${SMALL_WIDTH}) {
+    width: 40%;
+  }
 `;
 
 const SKILLS = {
   languages: ['JavaScript', 'Python', 'MUMPS'],
   frameworks: {
+    Languages: ['JavaScript', 'Python', 'MUMPS'],
+    'Other': ['Docker', 'AWS', 'Git', 'Jest, Enzyme', 'Webpack', 'Mocha, Chai', 'New Relic', 'Loader.io'],
     'Front End': ['React', 'React-Native', 'Redux', 'React Router', 'jQuery', 'HTML', 'CSS'],
     'Back End': ['Node', 'Express', 'Nginx', 'SQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Redis'],
-    'Other': ['Docker', 'AWS', 'Git', 'Jest, Enzyme', 'Webpack', 'Mocha, Chai', 'New Relic', 'Loader.io'],
   },
 };
 
-const Card = ({ skillset, skills, reverse }) => {
-  const [animState, setAnimState] = useState(0);
+const List = ({ skillset, skills, reverse }) => {
+  const [animState, setAnimState] = useState(false);
   const ref = useRef();
 
   useEffect(() => {
     let options = {
       root: null,
       rootMargin: '0px',
-      threshold: 1,
+      threshold: 0,
     }
 
     const intersectionCb = (entries) => {
       entries.forEach((entry) => {
-        if (entry.target === ref.current) setAnimState(curr => {
-          if (curr === 0 && entry.isIntersecting) return curr + 1;
-          return curr;
+        if (entry.target === ref.current) setAnimState(curr => curr || entry.isIntersecting);
         });
-      });
-    }
+    };
 
     const observer = new IntersectionObserver(intersectionCb, options);
     observer.observe(ref.current);
   }, []);
 
   return (
-    <React.Fragment>
-      {animState < 2 && (
-          <SkillSet
-            ref={ref}
-            className={animState && 'active'}
-            onAnimationEnd={() => setAnimState(curr => curr + 1)}
-            >
-            <SkillSetTitle>{skillset}</SkillSetTitle>
-          </SkillSet>
-      )}
-      {animState > 1 && (
-          <SkillSet flip={true} className={animState && 'active'}>
-            <SkillSetTitle>{skillset}</SkillSetTitle>
-            <Skills>{skills.join(', ')}</Skills>
-          </SkillSet>
-      )}
-    </React.Fragment>
+    <SkillSet ref={ref} className={animState && 'active'}>
+      <div>
+        <SkillSetTitle>{skillset}</SkillSetTitle>
+        {skills.map((skill, idx) => <Skills key={idx} delay={idx}>{skill}</Skills>)}
+      </div>
+    </SkillSet>
   );
 };
 
@@ -139,12 +120,11 @@ export default () => {
   return (
     <Container>
       <Title>Here's What I Bring</Title>
-      <SubTitle><b>Languages:</b> {SKILLS.languages.join(', ')}</SubTitle>
-      <Groups>
+      <SkillSets>
         {Object.entries(SKILLS.frameworks).map(([skillset, skills], idx) => (
-          <Card key={skillset} {...{ skillset, skills }} />
+          <List key={skillset} {...{ skillset, skills }} />
         ))}
-      </Groups>
+      </SkillSets>
     </Container>
   )
 }
