f3d18a8778f7ac496b5e0cad86e48c0297dc6c09
joshua-t-liu <josh.th.liu@gmail.com>
2020-08-31 09:52:55 -0700

    reordered skills

--- client/skill_data.js
+++ client/skill_data.js
@@ -2,8 +2,8 @@ export default {
   languages: ['JavaScript', 'Python', 'MUMPS'],
   frameworks: {
     Languages: ['JavaScript', 'MUMPS'],
-    'Other': ['Docker', 'AWS', 'Git', 'Jest', 'Enzyme', 'Webpack', 'Babel', 'Mocha', 'Chai', 'New Relic', 'Loader.io'],
-    'Front End': ['React', 'React-Native', 'Redux', 'React Router', 'jQuery', 'HTML', 'CSS'],
-    'Back End': ['Node', 'Express', 'Nginx', 'NoSQL', 'SQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Redis'],
+    Frontend: ['React', 'React-Native', 'Redux', 'React Router', 'jQuery', 'HTML', 'CSS'],
+    Backend: ['Node', 'Express', 'Nginx', 'NoSQL', 'SQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Redis'],
+    Other: ['Docker', 'AWS', 'Git', 'Jest', 'Enzyme', 'Webpack', 'Babel', 'Mocha', 'Chai', 'New Relic', 'Loader.io'],
   },
 };
