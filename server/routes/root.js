ff800cb70c252a1aa29729d80ae36dfe153c48d8
Ubuntu <ubuntu@ip-172-31-50-126.ec2.internal>
2020-12-05 18:21:03 +0000

    fixed defualt route

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/easychart', (req, res) => res.redirect('/chartsy'));
 
-router.get(/\/portfolio|\/photos|\/mapView|\/streetView|\/schools|\/commute/, (req, res) => {
-  res.redirect('/');
-});
-
 router.all('*', (req, res) => {
-  res.sendFile('/home/joshua/personal/dist/index.html');
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
 });
 
 module.exports = router;
